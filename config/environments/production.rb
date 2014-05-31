05d38384a5bff722e7614379d61656b310d1c5be
Jeremy Samples <judesamp@gmail.com>
2014-05-31 18:03:52 -0400

    added mail config to production env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,5 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.action_mailer.default_url_options = { :host => 'stormy-garden-7330.herokuapp.com' }
   # Code is not reloaded between requests.
   config.cache_classes = true
 
@@ -80,4 +79,19 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.default_url_options = { :host => 'http://stormy-garden-7330.herokuapp.com/' }
+
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com', 
+    port: 587, 
+    domain: 'gmail.com',
+    user_name: "tiyappmailer",
+    password: "theironyard",
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_options = { from: 'no-reply@example.com' }
 end
