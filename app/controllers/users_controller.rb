53bac1c8073526b1e61893bbb7b94e3e6f058bce
Jeremy Samples <judesamp@gmail.com>
2014-06-05 16:21:38 -0400

    fixes judesamp/ironbase#11

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ require 'digest/md5'
 class UsersController < ApplicationController
   authorize_resource
   before_action :authenticate_user!
-  before_filter :ensure_admin, only: [:course_dashboard, :cohort_dashboard]
+  #before_filter :ensure_admin, only: [:course_dashboard, :cohort_dashboard]
 
   def dashboard
     #query this in a different way later
@@ -22,26 +22,11 @@ class UsersController < ApplicationController
   def add_user_to_cohort
     user = User.find(params[:users][:user_id])
     @cohort = Cohort.find(params[:users][:cohort_id])
-    enrollment_relation = Enrollment.where("cohort_id = ? AND user_id = ?", @cohort.id, user.id)
-    enrollment = enrollment_relation.first
-    if @cohort.users.include? user
-      if enrollment.status == "active"
-        respond_to do |format|
-          format.js {render plain: '0'}
-        end
-      else
-        enrollment.status = "active"
-        enrollment.save
-        @active_cohort_users = @cohort.users.joins(:enrollments).where("enrollments.status" => "active")
-        respond_to do |format|
-          format.js
-        end
-      end
-    else
-
-      @cohort.users << user unless @cohort.users.include?(user)
+    @cohort.users << user unless @cohort.users.include?(user)
+    enrollment = find_enrollment(params[:users][:cohort_id], params[:users][:user_id]) 
+    enrollment.status = "active"
+    if enrollment.save!
       @active_cohort_users = @cohort.users.joins(:enrollments).where("enrollments.status" => "active")
-
       respond_to do |format|
         format.js
       end
@@ -50,8 +35,7 @@ class UsersController < ApplicationController
 
   def remove_user_from_cohort
     @cohort = Cohort.find(params[:users][:cohort_id])
-    enrollment_relation = Enrollment.where("cohort_id = ? AND user_id = ?", params[:users][:cohort_id], params[:users][:user_id])
-    enrollment =  enrollment_relation.first
+    enrollment = find_enrollment(params[:users][:cohort_id], params[:users][:user_id]) 
     enrollment.status = "inactive"
     if enrollment.save!
       @active_cohort_users = @cohort.users.joins(:enrollments).where("enrollments.status" => "active")
@@ -81,6 +65,10 @@ class UsersController < ApplicationController
       gflash notice: "This is not the page you are looking for."
       redirect_to root_path
     end
+  end
+
+  def find_enrollment(cohort_id, user_id)
+    Enrollment.find_by("cohort_id = ? AND user_id = ?", cohort_id, user_id)
   end
   
 end
